a35fe5802843a6236f86cec05879600e5640ce13
sumit <yessumitsingh007@gmail.com>
2025-10-14 08:49:37 +0530

    fix resend api key name

--- src/app/api/sendEmail/route.ts
+++ src/app/api/sendEmail/route.ts
@@ -6,7 +6,7 @@ export async function POST(req: Request) {
     try {
         const body = await req.json();
         const {to,subject,html} = body;
-        resend.emails.send({
+        await resend.emails.send({
             from: 'onboarding@resend.dev',
             to: 'yessumitsingh007@gmail.com',
             subject: subject,
